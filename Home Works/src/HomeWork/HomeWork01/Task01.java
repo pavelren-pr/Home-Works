462a9741b359152a0ab223a5d7a9bd17d8ca0832
pavelren-pr <pavelren.pr@gmail.com>
2025-07-05 22:56:51 +0300

    Update Task01.java

--- Home Works/src/HomeWork/HomeWork01/Task01.java
+++ Home Works/src/HomeWork/HomeWork01/Task01.java
@@ -4,7 +4,7 @@ public class Task01 {
     public static void main(String[] args) {
         int x = 10, y = 3;
         double z = 192.3456;
-        System.out.println(x + "   " + y + "   " + z + "   " + (x + y));
+        System.out.println(x + " " + y + " " + z + " " + (x + y));
     }
 }
 
